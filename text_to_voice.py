37987133899d7b3c5cbdb09d7adc80c7aad6acd1
Jainil570 <154754994+Jainil570@users.noreply.github.com>
2024-10-21 18:12:38 +0530

    Update text_to_voice.py

--- text_to_voice.py
+++ text_to_voice.py
@@ -5,7 +5,7 @@ import platform
 import subprocess
 
 # The text that you want to convert to audio in Gujarati
-mytext = 'તમે ગીક્સફોર્ગીક્સ પર સ્વાગત છે જો!'
+mytext = 'SignAura માં સ્વાગત છે'
 
 # Language in which you want to convert (Gujarati)
 language = 'gu'
